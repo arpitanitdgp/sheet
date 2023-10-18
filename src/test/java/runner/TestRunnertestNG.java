f30c62a0b1e11eccd4dee0eb08d76bf953101f4f
SheetalKarotiya <sheetalwce1@gmail.com>
2023-10-18 18:08:14 -0400

    2nd last commit

--- src/test/java/runner/TestRunnertestNG.java
+++ src/test/java/runner/TestRunnertestNG.java
@@ -1,19 +1,29 @@
 package runner;
 
 
-
+import org.junit.runner.RunWith;
 import org.testng.annotations.DataProvider;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;   
+
+/*import org.testng.annotations.DataProvider;
 
 
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+*/
 
-
-
+@RunWith(Cucumber.class)
 @CucumberOptions( 
 		monochrome = false,
-	    plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm","json:target/cucumber.json"},
+	    plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
+	    		    "html:target/cucumber-reports/dsalgo.html",
+	               "json:target/cucumber-reports/Cucumber.json",
+	    		   "junit:target/cucumber-reports/Cucumber.xml"},
+	   
 	    
 		features = { "src/test/java/Myfeatures"}, // location of feature files
 		glue = "stepdefinationsTest") // location of step definition files
