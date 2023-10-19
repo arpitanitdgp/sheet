30f500311cf84cb7643d8402a681dc06629c75c8
SheetalKarotiya <sheetalwce1@gmail.com>
2023-10-19 01:25:57 -0400

    changes in pom

--- src/test/java/runner/TestRunnertestNG.java
+++ src/test/java/runner/TestRunnertestNG.java
@@ -8,22 +8,19 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;   
 
-/*import org.testng.annotations.DataProvider;
-
-
-
-import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.cucumber.testng.CucumberOptions;
-*/
+   
 
 @RunWith(Cucumber.class)
 @CucumberOptions( 
 		monochrome = false,
 	    plugin = { "pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm",
-	    		    "html:target/cucumber-reports/dsalgo.html",
-	               "json:target/cucumber-reports/Cucumber.json",
-	    		   "junit:target/cucumber-reports/Cucumber.xml"},
-	   
+	    		    "html:target/cucumber-reports.html",
+	    		        "json:target/cucumber-reports/Cucumber.json",
+	    		   "junit:target/cucumber-reports/Cucumber.xml",
+	    		   "com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:"},
+	    		   
+	    		  
+	    		   
 	    
 		features = { "src/test/java/Myfeatures"}, // location of feature files
 		glue = "stepdefinationsTest") // location of step definition files
